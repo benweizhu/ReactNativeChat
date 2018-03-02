9f50092cdc0f46bfa4bacde717b4e1c9ed230463
benweizhu <benweizhu@gmail.com>
2018-03-02 15:50:28 +0800

    basic home structure

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default class Home extends Component {
   render() {
     return (
-      <View>
-        <View>head</View>
-        <View>body</View>
-        <View>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Header</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>Body</Text>
+        </View>
+        <View style={styles.footer}>
           <TouchableOpacity>
             <View>
               <Text>Let's chat</Text>
@@ -18,3 +22,18 @@ export default class Home extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    flex: 1
+  },
+  body: {
+    flex: 3
+  },
+  footer: {
+    flex: 1
+  }
+});
