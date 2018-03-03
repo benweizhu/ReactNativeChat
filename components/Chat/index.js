7da7517031e1c40b8e8f4851267b226f8f180e75
benweizhu <benweizhu@gmail.com>
2018-03-03 11:31:21 +0800

    simple integrate with react gifted chat

--- components/Chat/index.js
+++ components/Chat/index.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import React from "react";
+import { GiftedChat } from "react-native-gifted-chat";
 
-export default class Chat extends Component {
+export default class Chat extends React.Component {
+  state = {
+    messages: []
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://facebook.github.io/react/img/logo_og.png"
+          }
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
     );
   }
 }
